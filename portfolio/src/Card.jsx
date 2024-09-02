4ff154de67ecb4b26fe0dcfe7897410953aadf75
Ghalib <ghalibfaruqe@duck.com>
2024-09-03 00:43:07 +0600

    initial

---
+++ portfolio/src/Card.jsx
@@ -0,0 +1,20 @@
+import './card.css'
+import profilePic from './assets/profilePic.jpeg';
+function Card(){
+    return(
+        <> 
+        <div className="bar">
+
+        <div className="card">
+        <img className="profileImage" src={profilePic} alt="Profile Picture" />
+        <h2>Ghalib Faruqe</h2>
+        </div>
+        <div className="info">
+            <p>There are few issues that we need to fix</p>
+
+        </div>
+        </div>
+        </>
+    );
+}
+export default Card
---
+++ portfolio/src/Header.jsx
@@ -0,0 +1,23 @@
+import './nav.css';
+function Header(){
+    return(
+        <header>
+            
+            <nav>
+                <ul>
+                    <li> <a href="#Home">Home</a></li>
+                    <li> <a href="#work">Work</a></li>
+                    <li> <a href="#Review">Review</a></li>
+
+
+
+                </ul>
+            </nav>
+            <hr></hr>
+            
+           
+
+        </header>
+    );
+}
+export default Header
